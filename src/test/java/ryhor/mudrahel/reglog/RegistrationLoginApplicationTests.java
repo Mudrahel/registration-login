93eb8b649e9295c2be79bcd0207230df4bfc9f14
Ryhor_Mudrahel <ryhor_mudrahel@epam.com>
2023-04-24 19:45:07 -0400

    Initial commit

---
+++ src/test/java/ryhor/mudrahel/reglog/RegistrationLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package ryhor.mudrahel.reglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
